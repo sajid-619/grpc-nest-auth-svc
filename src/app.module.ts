b8629cd52919fdd2dfda92eb4e00d419bdbff000
sajid-619 <zsajid014@gmail.com>
2022-12-18 14:55:41 +0600

    Final Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
+      host: '<YOUR_POSTGRES_LOCALHOST_ADDRESS>',
+      port: '<YOUR_POSTGRES_PORT>',
       database: 'micro_auth',
-      username: 'sz619',
-      password: 'root',
+      username: '<YOUR_POSTGRES_USERNAME>',
+      password: '<YOUR_POSTGRES_PASSWORD>',
       entities: ['dist/**/*.entity.{ts,js}'],
       synchronize: true, // never true in production!
     }),
